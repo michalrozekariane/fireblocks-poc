edb0f9c51d5c8acb20fd3ac25008df3843954d24
Michal Rozek <michal.rozek@arianelabs.com>
2025-07-29 09:41:19 +0200

    init2
    Signed-off-by: Michal Rozek <michal.rozek@arianelabs.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { deployERC3643Contract } from './fireblocks/contract-deploy';
-import { deployERC20F } from './fireblocks/erc20f-deploy';
 import { getVaultAccountDetails, listVaultAccounts } from './fireblocks/vaults';
 
 async function main() {
-  await deployERC20F()
   // await deployERC3643Contract()
   // await listVaultAccounts()
   // await getVaultAccountDetails('5')
